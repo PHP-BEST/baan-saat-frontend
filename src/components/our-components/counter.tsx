590651a3c916adc368b50624e651610eb33e1db0
BadLuckZ <thanagorn8802@gmail.com>
2025-08-18 22:02:09 +0700

    feat: add jest

---
+++ src/components/our-components/counter.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import useCounter from "@/hooks/useCounter";
+
+export default function Counter() {
+  const { count, increment, val, setVal } = useCounter();
+
+  return (
+    <div>
+      <Input
+        className="w-72"
+        type="text"
+        value={val}
+        onChange={(e) => {
+          const num = e.target.value.replace(/[^0-9]/g, "");
+          setVal(Math.max(0, Number(num)));
+        }}
+      />
+      <Button className="w-72" onClick={() => increment()}>
+        count is {count}
+      </Button>
+    </div>
+  );
+}
