3ef9def3bd279506ad1168f058591801d7a6c664
BadLuckZ <thanagorn8802@gmail.com>
2025-08-18 12:40:36 +0700

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1 className="text-2xl text-red-500 font-bold">Hello World!</h1>;
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center">
+      <h1 className="text-2xl text-red-500 font-bold">Hello World!</h1>
+      <Button
+        className="cursor-pointer"
+        variant={"outline"}
+        onClick={() => {
+          alert("Test Test 123");
+        }}
+      >
+        This is a button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
