326ba833f806f64df6fc58b3a46e4856f9b5448f
Bennette321321 <6631321321@student.chula.ac.th>
2025-08-26 23:28:59 +0700

    chore: add bai jamjuree

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import type React from 'react';
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen w-full bg-background">
+    <div className="min-h-screen w-full bg-background font-serif">
       {/* Add Other Layout Here */}
       {children}
     </div>
