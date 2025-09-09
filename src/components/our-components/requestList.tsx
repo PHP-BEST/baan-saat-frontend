b8638a08939056499a7489e23fe2b300f5fe6f57
Pete ang Jaaa <sakayanagi101@gmail.com>
2025-09-10 02:08:47 +0700

    feat: create component for Dialog

---
+++ src/components/our-components/requestList.tsx
@@ -0,0 +1,21 @@
+interface RequestListProps{
+    serviceName: string;
+    price: string;
+    date: string;
+}
+export default function RequestList ({
+    serviceName, 
+    price, 
+    date 
+}: RequestListProps){
+    return (
+        <div className="grid grid-cols-[3fr_1fr_1.5fr_0.5fr] px-3 my-1">
+        <span className="text-gray-600">{serviceName}</span>
+        <span className="text-gray-600">{price}</span>
+        <span className="text-gray-600">{date}</span>
+        <div className="flex items-end">
+        <input className="w-4 h-4 bg-gray-400 mb-0.5" type="checkbox" />
+        </div>
+        </div>
+    );
+}
