8b1c1b22bbc53db0cc9fc2000ac23485964591bc
Shinatip Pattanapitoon <shinatip.ptn@gmail.com>
2025-09-12 20:42:49 +0700

    hide tanstack query devtools in production

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './route';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { NODE_ENV } from './config/env';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +17,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {
+        NODE_ENV !== 'production' &&
+        <ReactQueryDevtools initialIsOpen={false} />
+      }
     </QueryClientProvider>
   );
 }
