1998a19a753a9cf21420a64cff72f15fc394e0d8
Pete ang Jaaa <sakayanagi101@gmail.com>
2025-11-16 01:53:32 +0700

    add price date to offer interface

--- src/interfaces/Offer.ts
+++ src/interfaces/Offer.ts
@@ -7,6 +7,7 @@ export interface Offer {
   customerId: string;
   providerId: string;
   date: Date;
+  price: number;
   status: OfferedStatus;
   description?: string;
   createdAt: Date;
@@ -20,9 +21,16 @@ export interface OfferDetail {
   customer: User;
   customerId: string;
   provider: User;
+  price: number;
   providerId: string;
+  date: Date;
   status: OfferedStatus;
   createdAt: Date;
   updatedAt: Date;
 }
-export type OfferedStatus = 'Pending' | 'Accepted' | 'Rejected';
+export type OfferedStatus =
+  | 'Pending'
+  | 'Accepted'
+  | 'Rejected'
+  | 'Taken'
+  | 'Deleted';
